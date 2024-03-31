5257f4686b6344d7de14193a49e6d07fa7611af4
lgutierrezramirez <lgutierrezramirez@switchfly.com>
2024-03-31 11:16:08 -0600

    add logic to include subdirectories

--- rust_projects_cleaner.sh
+++ rust_projects_cleaner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-work_dir=$(dirname "$(readlink --canonicalize-existing "${0}" 2> /dev/null)")
+readonly work_dir=$(dirname "$(readlink --canonicalize-existing "${0}" 2> /dev/null)")
 readonly current_dir="${PWD}"
 
 if [[ -z "${RUST_PROJECTS_DIR}" ]]; then
@@ -8,18 +8,8 @@ if [[ -z "${RUST_PROJECTS_DIR}" ]]; then
     exit 1
 fi
 
-cd "${RUST_PROJECTS_DIR}"
-
-for dir in *; do
-    rust_project="${RUST_PROJECTS_DIR}/${dir}"
-    if [ -d "${rust_project}" ]; then
-        if [[ -f "${rust_project}/Cargo.toml" ]]; then
-            cd "${RUST_PROJECTS_DIR}/${dir}"
-            cargo clean
-        fi
-    fi
-done
-
-cd "${current_dir}"
+while read dir; do
+    cargo clean --manifest-path "${dir}/Cargo.toml"
+done < <(find "${RUST_PROJECTS_DIR}" -type d -exec test -e '{}/Cargo.toml' \; -print)
 
 exit 0
